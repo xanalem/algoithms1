268749fb7ab1d64681d0d8a674edbc3311ebd4a9
Brandon S Navone <bn03907@srcs.loc>
2017-12-11 11:50:41 -0800

    hello

--- Loops/loops.js
+++ Loops/loops.js
@@ -14,3 +14,80 @@ function factorial(number) {
     }
     return result;
 }
+
+// Multiplies all the elements of an array by a number
+function multiply(a, b) {
+    for (i = 0; i < a.length; i++) {
+        a[i] = a[i] * b;
+        console.log(a[i]);
+    }
+    return a;
+}
+
+// Identifies all elements of an array as either even or odd
+function evenOrOdd(numbers) {
+    var results = [];
+    for (i = 0; i < numbers.length; i++) {
+        if (Number.isInteger(numbers[i])) {
+            if (numbers[i] % 2 == 0) {
+                results[i] = "even";
+            } else {
+                results[i] = "odd";
+            }
+        } else {
+            results[i] = "Not an integer.";
+        }
+        console.log(results);
+    }
+    return results;
+}
+
+// Identifies all elements of an array as either even or odd
+function evenOrOdd(numbers) {
+    var results = [];
+    for (i = 0; i < numbers.length; i++) {
+        if (Number.isInteger(numbers[i])) {
+            if (numbers[i] % 2 == 0) {
+                results[i] = "even";
+            } else {
+                results[i] = "odd";
+            }
+        } else {
+            // break out here
+            break;
+        }
+        console.log(results);
+    }
+    return results;
+}
+
+// Prints a stack of characters
+function stack(num) {
+    var sout = "";
+    for (i = 1; i <= num; i++) {
+        sout = sout + "<br/>";
+        for (j = 1; j <= i; j++) {
+            sout = sout + " * ";
+        }
+    }
+    return sout;
+}
+
+// Example of while loop
+function loopUntilTen() {
+    var i = 0;
+    while (i < 10) {
+        i++;
+        console.log("The number is " + i);
+    }
+}
+
+// Example of do loop
+function loopUntilTen() {
+    var i = 0;
+    do {
+        i++;
+        console.log("The number is " + i);
+    }
+    while (i < 10);
+}
